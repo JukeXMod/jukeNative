ea38950e4de26aceef17f89e9bd74ef5ec83e4d6
ted sandridge <sand0959@gmail.com>
2017-08-02 16:26:06 -0400

    view added along with mongodb stuff, not finished

---
+++ app/components/Login/styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet, Dimensions } from "react-native";
+let { width } = Dimensions.get('window');
+
+export default iconStyles = StyleSheet.create({
+	borderRadius: 10,
+	iconStyles: { paddingVertical: 5 },
+});
+
+export default styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#FFF',
+
+	},
+	content: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: 20,
+	},
+	avatarImage: {
+		borderRadius: 50,
+		height: 100,
+		width: 100,
+
+	},
+	header: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	text: {
+		textAlign: 'center',
+		color: "#333",
+		marginBottom: 5,
+	},
+	buttons: {
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		margin: 20,
+		marginBottom: 30,
+	}
+});
