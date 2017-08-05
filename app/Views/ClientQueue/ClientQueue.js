3832d5c75f115c2e1844569b065967985a5d4364
Noah King <noahking@MR01_Blackwell_ATC.car01.drhm.nc.frontiernet.net>
2017-08-05 12:47:44 -0400

    adding list

--- app/Views/ClientQueue/ClientQueue.js
+++ app/Views/ClientQueue/ClientQueue.js
@@ -1,64 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, AppRegistry, View, Image} from 'react-native';
-import {Button} from 'react-native-elements';
-import { TabNavigator} from 'react-navigation';
+import { StyleSheet, Text, AppRegistry, View, Image, TouchableOpacity, FlatList, Dimensions } from 'react-native';
+// import SpotifySoundCloud from '../components/SpotifySoundCloud/SpotifySoundCloud.js';
+import Toolbar from '../../components/Toolbar/Toolbar.js';
+import { StackNavigator } from 'react-navigation';
+import { List, ListItem } from 'react-native-elements';
 
-export default class ClientQueue extends React.Component {
-  static navigationOptions = {
-    tabBarLabel: 'Home',
-    // Note: By default the icon is only shown on iOS. Search the showIcon option below.
-    tabBarIcon: ({ tintColor }) => (
-      <Image
-        source={require("../../images/disc.png")}
-        style={[styles.icon, {tintColor: tintColor}]}
-      />
-    ),
-  };
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+]
+
+
+export default class ClientQueueList extends React.Component {
 
   render() {
     return (
-      <Button
-        onPress={() => this.props.navigation.navigate('Home')}
-        title="Go to notifications"
+
+        <List containerStyle={{marginBottom: 30}}>
+  {
+    list.map((l, i) => (
+      <ListItem
+        roundAvatar
+        avatar={{uri:l.avatar_url}}
+        key={i}
+        title={l.name}
       />
+    ))
+  }
+</List>
     );
   }
 }
-
-class MyNotificationsScreen extends React.Component {
-  static navigationOptions = {
-    tabBarLabel: 'Notifications',
-    tabBarIcon: ({ tintColor }) => (
-      <Image
-        source={require("../../images/disc.png")}
-        style={[styles.icon, {tintColor: tintColor}]}
-      />
-    ),
-  };
-
-  render() {
-    return (
-      <Button
-        onPress={() => this.props.navigation.goBack()}
-        title="Go back home"
-      />
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  icon: {
-    width: 26,
-    height: 26,
+let {width, height} = Dimensions.get('window')
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: width,
+    height: height,
+    backgroundColor:'transparent',
+    
   },
-});
+  
 
-const MyApp = TabNavigator({
-  Notifications: {
-    screen: MyNotificationsScreen,
-  }
-}, {
-  tabBarOptions: {
-    activeTintColor: '#e91e63',
-  },
 });
